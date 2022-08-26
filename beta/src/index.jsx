9715897c086bf41815e5b8640770311ad0ff9834
theboringschool <theboringschool.org@gmail.com>
2022-08-26 20:30:08 +0545

    Review request from Gaire

--- beta/src/index.jsx
+++ beta/src/index.jsx
@@ -3,10 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import 'remixicon/fonts/remixicon.css'
 import './index.css'
+import '../src/utils/RPC'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <>
       <App />
-  </React.StrictMode>
+  </>
 );

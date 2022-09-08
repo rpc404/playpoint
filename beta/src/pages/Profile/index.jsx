c1370c3ba2f402a2a3f61f8ba229521320c43c5b
theboringschool <theboringschool.org@gmail.com>
2022-09-08 18:45:01 +0545

    Working on profile

--- beta/src/pages/Profile/index.jsx
+++ beta/src/pages/Profile/index.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import './style.css'
 
 export default function Profile() {
   return (
-    <div className="profile__container">Profile</div>
+    <div className="profile__container">
+      <div className="summary__container">
+        <div>
+          <i></i>
+          <div>
+            <p>Your money in pool</p>
+            h2
+          </div>
+        </div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
   )
 }

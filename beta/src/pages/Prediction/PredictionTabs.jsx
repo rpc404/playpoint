da96f18ae17ff22dc9f06aded8e9494d09fdce66
theboringschool <theboringschool.org@gmail.com>
2022-09-08 10:57:42 +0545

    Working on Prediction

---
+++ beta/src/pages/Prediction/PredictionTabs.jsx
@@ -0,0 +1,186 @@
+import * as React from "react";
+import PropTypes from "prop-types";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import { Button } from "@mui/material";
+import Checkbox from "@mui/material/Checkbox";
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
+}
+
+export default function PredictionTabs() {
+  const [value, setValue] = React.useState(0);
+  const [activeAmount, setActiveAmount] = React.useState(5);
+  const [activeQuestionaire, setActiveQuestionaire] = React.useState(3);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box sx={{ width: "100%" }} className="predictionTabs__container">
+      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          aria-label="basic tabs example"
+        >
+          <Tab label="Unlimited" {...a11yProps(0)} />
+          <Tab label="Duo (2)" {...a11yProps(1)} />
+          <Tab label="Trio (3)" {...a11yProps(2)} />
+          <Tab label="Nonet (9)" {...a11yProps(3)} />
+        </Tabs>
+      </Box>
+      <TabPanel value={value} index={0}>
+        <div className="topBar">
+          <div>
+            <div>Price:</div>
+            <Button className={activeAmount === 5 && "active"}>$5</Button>
+            <Button className={activeAmount === 20 && "active"}>$20</Button>
+            <Button className={activeAmount === 50 && "active"}>$50</Button>
+          </div>
+          <div>
+            <div>Questionaires:</div>
+            <Button className={activeQuestionaire === 3 && "active"}>3</Button>
+            <Button className={activeQuestionaire === 4 && "active"}>4</Button>
+          </div>
+        </div>
+
+        <div className="questionaires">
+          <div className="questionItem">
+            <div className="top">
+              <p>
+                1. Which of the two teams leads in the first half of the match?
+              </p>
+              <p>20 Points</p>
+            </div>
+            <div className="answers">
+              <div className="label">
+                <Checkbox />
+                Sweden
+              </div>
+              <div className="label">
+                <Checkbox />
+                Draw
+              </div>
+              <div className="label">
+                <Checkbox />
+                Brazil
+              </div>
+            </div>
+          </div>
+          <div className="questionItem">
+            <div className="top">
+              <p>
+                2. Which of the two teams leads in the first half of the match?
+              </p>
+              <p>30 Points</p>
+            </div>
+            <div className="answers">
+              <div className="label">
+                <Checkbox />
+                Sweden
+              </div>
+              <div className="label">
+                <Checkbox />
+                Draw
+              </div>
+              <div className="label">
+                <Checkbox />
+                Brazil
+              </div>
+            </div>
+          </div>
+          <div className="questionItem">
+            <div className="top">
+              <p>
+                3. Which of the two teams leads in the first half of the match?
+              </p>
+              <p>50 Points</p>
+            </div>
+            <div className="answers">
+              <div className="label">
+                <Checkbox />
+                Sweden
+              </div>
+              <div className="label">
+                <Checkbox />
+                Draw
+              </div>
+              <div className="label">
+                <Checkbox />
+                Brazil
+              </div>
+            </div>
+          </div>
+          {activeQuestionaire === 4 && (
+            <div className="questionItem">
+              <div className="top">
+                <p>
+                  4. Which of the two teams leads in the first half of the
+                  match?
+                </p>
+                <p>20 Points</p>
+              </div>
+              <div className="answers">
+                <div className="label">
+                  <Checkbox />
+                  Sweden
+                </div>
+                <div className="label">
+                  <Checkbox />
+                  Draw
+                </div>
+                <div className="label">
+                  <Checkbox />
+                  Brazil
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Item Two
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        Item Two
+      </TabPanel>
+      <TabPanel value={value} index={3}>
+        Item Two
+      </TabPanel>
+    </Box>
+  );
+}
--- beta/src/pages/Prediction/index.jsx
+++ beta/src/pages/Prediction/index.jsx
@@ -1,7 +1,107 @@
-import React from 'react'
+import React from "react";
+import { Helmet } from "react-helmet";
+import "./style.css";
+import { Button } from "@mui/material";
+import Identicon from "react-identicons";
+import { LineChart, LinearXAxis, LineSeries } from "reaviz";
+import PredictionTabs from "./PredictionTabs";
 
 export default function Prediction() {
   return (
-    <div>Prediction</div>
-  )
+    <div className="prediction__container">
+      <Helmet>
+        <title>Brazil vs Qatar | Prediction | Playpoint</title>
+      </Helmet>
+
+      <div className="main__container">
+        <div className="recentPredictions">
+          <h3>Active Predictions</h3>
+
+          <div className="prediction__items">
+            {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((data) => (
+              <div className="predictedCard__container" key={data}>
+                <Identicon size={32} string="randomness" />
+                <div>
+                  <div className="details">
+                    <Button>View Answer</Button>
+                    <p>Duo</p>
+                  </div>
+                  <p>0x3ebfb8e...0b9c4 predicted on Sweden vs Netherlands.</p>
+                  <div className="info">
+                    <p>$5~0.075 PPTT</p>
+                    <p>2m Ago</p>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="predictionTable">
+          <div className="predictionTable__topBar">
+            <div className="predictionTable__competitor">
+              <div>
+                <p>Sweden</p>
+                <img
+                  src="https://www.kindpng.com/picc/m/297-2972143_sweden-logo-png-transparent-png.png"
+                  alt=""
+                  loading="lazy"
+                />
+              </div>
+              <span>vs</span>
+              <div>
+                <img
+                  src="https://seeklogo.com/images/F/flag-of-brazil-logo-94D3E82278-seeklogo.com.png"
+                  alt=""
+                  loading="lazy"
+                />
+                <p>Brazil</p>
+              </div>
+            </div>
+
+            <div className="marketInfo">
+              <div>
+                <p>24h Volume (PPTT)</p>
+                <p>12,233,333</p>
+              </div>
+              <div>
+                <p>Total Predictions</p>
+                <p>1,233,455</p>
+              </div>
+            </div>
+          </div>
+          <div className="predictionTable__mainContainer">
+            <LineChart
+              className="graphData"
+              width="90%"
+              height={300}
+              data={[
+                { key: new Date("11/25/2019"), data: 30 },
+                { key: new Date("11/29/2019"), data: 14 },
+                { key: new Date("11/30/2019"), data: 5 },
+                { key: new Date("12/01/2019"), data: 40 },
+                { key: new Date("12/02/2019"), data: 20 },
+              ]}
+              series={
+                <LineSeries
+                  colorScheme={(_data, _index, active) =>
+                    active ? "#2ecc71" : "#2ecc71"
+                  }
+                />
+              }
+            />
+            <div className="eventDetails">
+              <p>
+              <i className="ri-calendar-todo-line"></i> Event Details: 22nd November, 2022
+              </p>
+              <p>
+              <i className="ri-bar-chart-2-line"></i> Pool Size: Unlimited
+              </p>
+            </div>
+            <PredictionTabs />
+          </div>
+        </div>
+        <div className="leaderboards">Leaderboards</div>
+      </div>
+    </div>
+  );
 }

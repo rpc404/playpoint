7e0aeaa208aafa50c1a234f5855ef265beb92df4
Suraj Gaire <eriag321@gmail.com>
2022-09-11 11:07:49 +0545

    imageKit added

--- beta/src/pages/Showcases/QuickView.jsx
+++ beta/src/pages/Showcases/QuickView.jsx
@@ -6,16 +6,12 @@ import { useNavigate } from "react-router-dom";
 export default function QuickView({ handleModalClose }) {
   const navigate = useNavigate()
   const [gameData, setGameData] = React.useState(null);
-  // const naviagtion = useNavigate();
 
   // eslint-disable-next-line
   const handleModal = () => {
     handleModalClose(false);
     document.body.style = "";
   };
-
-  const code = CountryFlags.map(i => i.code)
-  console.log(code[0])
 
   const generateId = (fixture)=>{
     return `${String(fixture.HomeTeam).toLowerCase()}-${String(fixture.AwayTeam).toLowerCase()}-${String(fixture.MatchNumber)}`
